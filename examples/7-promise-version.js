7c6018c691933c16fded0d96498f2831afe4059f
Daiwei <daiweilu123@gmail.com>
2015-05-27 22:59:32 -0400

    Add promise demo

--- examples/7-promise-version.js
+++ examples/7-promise-version.js
@@ -1,36 +1,41 @@
-var data;
+var Bluebird = require('bluebird');
+var fs = require('fs');
+var request = require('request');
+var s3 = require('../lib/s3');
+var database = require('../lib/database');
+var mailer = require('../lib/mailer');
 
-fs.readFile('data.json')
+Bluebird.promisifyAll(fs);
+var requestAsync = Bluebird.promisify(request);
+Bluebird.promisifyAll(database);
+Bluebird.promisifyAll(mailer);
 
+// Level 0
+fs.readFileAsync('data.json')
+  .bind({})
   .then(function (buf) {
-    data = JSON.parse(buf.toString());
-    return request(data.imageUrl);
+    this.data = JSON.parse(buf.toString());
+    return requestAsync(this.data.image_url);
   })
-
-  .then(function (body) {
+  .spread(function (res, body) {
     return s3.putObject({
       Bucket: 'tame-your-callback',
       ContentType: 'image/svg+xml',
       ACL: 'public-read',
-      Key: data.uploadUrl,
+      Key: this.data.upload_url,
       Body: body
     });
   })
-
   .then(function () {
-    return database.insert(data);
+    return database.insertAsync(this.data);
   })
-
   .then(function () {
-    return data.emails.map(function (email) {
-      return Mailer.send(data, email);
-    });
+    return this.data.emails;
   })
-
+  .each(function (email) {
+    return mailer.sendAsync(this.data, email);
+  })
   .then(function () {
     console.log('I am done!');
-  })
-
-  .catch(function (err) {
-    // Catch all previous errors
+    database.destroy();
   });
